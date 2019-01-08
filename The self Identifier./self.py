78a35e5f1321626f926392fa7ba08546fc3670c8
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2019-01-08 23:59:41 +0530

    Update The self Identifier./self.py

---
+++ The self Identifier./self.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  8 23:43:40 2019
+
+@author: abhijithneilabraham
+"""
+
+class CreditCard:
+    def __init__(self,customer,bank,acnt,limit):
+        self.customer=customer
+        self.bank=bank
+        self.account=acnt
+        self.limit=limit
+        self.balance=500
+    def customer_name(self):
+        
+        return self.customer
+    def bank_details(self):
+        return self.bank
+    def account_details(self):
+        self.account
+    def get_limit(self):
+        return self.limit
+    '''
+    self :
+
+self represents the instance of the class. 
+By using the "self" keyword we can access the attributes and methods of the class in python.
+
+__init__ :
+
+"__init__" is a reseved method in python classes.
+ It is known as a constructor in object oriented concepts. This method called when
+ an object is created from the class and it allow the class to initialize the attributes of a class.
+ '''
