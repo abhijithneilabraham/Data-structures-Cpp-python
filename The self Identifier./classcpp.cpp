ec69b000e72dcaaa6091eacf6f2c06b4351b981e
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2019-01-09 20:30:48 +0530

    finally finished

--- The self Identifier./classcpp.cpp
+++ The self Identifier./classcpp.cpp
@@ -1,18 +1,28 @@
 #include<iostream>
+
+using namespace std;
   class creditcard{
-  private:
+  private :
     string customer;
     string bank;
     string account;
     int limit;
+ int balance;
+public:
+ creditcard(): customer("Abhijith"),account("12002100"),bank("SBI"),limit(10000),balance(500){}
 
-  public:
-    string getname() const;
-    string account_details() const;
-    string bank_details() const;
-    string get_limit() const;
-  }
-string creditcard::getname()
-{
-  
+ int charge(int pay){
+ balance=balance-pay;
+ return balance;
+ }
+};
+
+int main(){
+
+creditcard c1;
+
+  int bal;
+  bal=c1.charge(100);
+  cout<<"balance"<<bal;
+  return 0;
 }
