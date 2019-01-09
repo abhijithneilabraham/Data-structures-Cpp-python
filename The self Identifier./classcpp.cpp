c9ca00014f8d4b7aaad927ab160abd5d8ed8499c
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2019-01-09 18:51:05 +0530

    cpp update

---
+++ The self Identifier./classcpp.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+  class creditcard{
+  private:
+    string customer;
+    string bank;
+    string account;
+    int limit;
+
+  public:
+    string getname() const;
+    string account_details() const;
+    string bank_details() const;
+    string get_limit() const;
+  }
+string creditcard::getname()
+{
+  
+}
