2fb4c5741967ed768077979878c0fd2109901bf7
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-01-11 03:02:54 +0530

    finish

--- Binary Search/Binary.cpp
+++ Binary Search/Binary.cpp
@@ -8,4 +8,22 @@ bool Binary(int data[],int target,int low,int high)
 if(low>high){
   return false;
 }
+else{
+  if(target==data[mid]){
+    return true; }
+    else if(target<data[mid]){
+      return Binary(data,target,low,mid-1);    }
+      else{
+        return Binary(data,target,mid+1,high);
+      }
+
+  }
+}
+int main()
+{
+int dat[]={1,8,4,5,3,2,4};
+bool b;
+b=Binary(dat,7,0,6);
+cout<<b;
+return 0;
 }
