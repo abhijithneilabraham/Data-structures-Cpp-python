09de8ed49d4d527dff722a1482404c287447ec6e
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-01-19 00:32:31 +0530

    finish

--- Array based sequences/cryptography.py
+++ Array based sequences/cryptography.py
@@ -17,13 +17,30 @@ class CaesarCipher:
             it starts from D ,if shift is given value 3
             '''
             decoder[k]=chr((k-shift)%26 + ord('A'))
-            self.forward=''.join(encoder)
-            self.backward=''.join(decoder) #both of these lines just joining the characters into string.
-            '''
-            we can only  manipulate the characters,since string is immutable
-            '''
+        self.forward=''.join(encoder)
+        self.backward=''.join(decoder) #both of these lines just joining the characters into string.
+        '''
+        we can only anipulate the characters ,since the string is immutable
+        '''
+
     def transform(self,original,code):
         msg=list(original)
         for k in range(len(msg)):
             if msg[k].isupper():
-                j
+                j=ord(msg[k])-ord('A')
+                msg[k]=code[j]
+                return ''.join(msg)
+            
+    def encrypt(self,message):
+        return self.transform(message,self.forward)
+    def decrypt(self,message):
+        return self.transform(message,self.backward)
+if __name__=='__main__':
+    cipher=CaesarCipher(3)
+    message="THE NAME IS ABHIJITH.AND IT IS HERE TO STAY."
+    coded=cipher.encrypt(message)
+    print(coded)
+    answer=cipher.decrypt(coded)
+    print(answer)
+
+                
