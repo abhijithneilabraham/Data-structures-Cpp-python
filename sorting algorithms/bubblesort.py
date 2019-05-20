7330620adc11f71138bec5597d0f81fc411b4fd1
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-05-20 23:49:57 +0530

    bubblesort updated

---
+++ sorting algorithms/bubblesort.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 20 23:35:47 2019
+
+@author: abhijithneilabraham
+"""
+
+a=[3,4,1,2,5,6]
+
+
+def bubblesort(a):
+    x=-1
+    flag=1
+    while flag:
+        flag=0
+        x+=1
+        for i in range(1,len(a)-x):
+            if a[i]<a[i-1]:
+                a[i],a[i-1]=a[i-1],a[i]
+                flag=1
+    return a
+print(bubblesort(a))
