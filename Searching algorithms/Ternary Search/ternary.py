b3c48eec564ea86ad912c64182d0da5d6f6a2591
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-06-11 16:56:41 +0530

    small change

--- Searching algorithms/Ternary Search/ternary.py
+++ Searching algorithms/Ternary Search/ternary.py
@@ -7,10 +7,6 @@ Created on Mon Jun 10 20:19:56 2019
 """
 def ternarysearch(data,target):
     left=0
-    right=len(arr)-1
-    while left<=right:
-        
-        temp=left+(right-left)//3
         
         
     
