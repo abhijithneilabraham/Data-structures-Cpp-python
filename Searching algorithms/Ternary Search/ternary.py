160367deaf8df68998e17bb932b420e1fc2d7be1
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-06-12 16:53:24 +0530

    next update

--- Searching algorithms/Ternary Search/ternary.py
+++ Searching algorithms/Ternary Search/ternary.py
@@ -5,5 +5,10 @@ Created on Mon Jun 10 20:19:56 2019
 
 @author: abhijithneilabraham
 """
-def ternarysearch(data,target,low,high):
+def ternarysearch(data,target):
+    left=0
+    right=len(arr)-1
+            
+        
+        
     
