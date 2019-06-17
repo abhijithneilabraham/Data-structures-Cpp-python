c9201572640eeaad976857b587b55d74d61b60b4
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-06-17 11:26:07 +0530

    Update Linked Llsts/creation.py

---
+++ Linked Llsts/creation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 17 11:23:19 2019
+
+@author: abhijithneilabraham
+"""
+
+class Node:
+    def __init__(self,dataval=None):
+        self.dataval=dataval
+        self.nextval=None
+        
