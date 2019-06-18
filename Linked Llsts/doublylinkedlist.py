fa5be7f3f6003d7ba83a1e61a5cb65f8b7f83926
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-06-18 19:20:07 +0530

    Update Linked Llsts/doublylinkedlist.py

--- Linked Llsts/doublylinkedlist.py
+++ Linked Llsts/doublylinkedlist.py
@@ -16,4 +16,20 @@ class doublylinked:
         self.head=None
     def pushtofront(self,data):
         newdata=Node(data)
-        
+        newdata.next=self.head
+        newdata.prev=None
+        if self.head is not None:
+            self.head.prev=newdata
+        self.head=newdata
+    def listprint(self):
+        printval=self.head
+        while printval is not None:
+            print(printval.dataval)
+            printval=printval.next
+list1=doublylinked()
+list1.head=Node("January")
+month2=Node("February")
+list1.head.next=month2
+list1.listprint()
+list1.pushtofront("December")
+list1.listprint()
