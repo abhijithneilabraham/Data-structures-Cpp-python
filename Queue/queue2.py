ee14fd39a73cf5aafd4e50da135e2fd896b52068
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-06-20 23:13:12 +0530

    Update Queue/queue2.py

--- Queue/queue2.py
+++ Queue/queue2.py
@@ -7,7 +7,17 @@ Created on Thu Jun 20 23:01:19 2019
 """
 
 class Queue:
-    def __init__(self,dataval):
-        self.dataval=dataval
-        self.next=None
-        
+    def __init__(self):
+        self.data=[]
+    def insertq(self,dataval):
+        if dataval not in self.data:
+            self.data.insert(0,dataval)
+            return True
+        return False
+    def peek(self):
+        return self.data
+q=Queue()
+q.insertq("Monday")
+q.insertq("Tuesday")
+q.insertq("wednesday")
+print(q.peek()) 
