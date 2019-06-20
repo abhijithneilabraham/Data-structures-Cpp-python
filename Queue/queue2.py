38834e27a494ac8519e7a5e54e0103b192ead5d5
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-06-20 23:03:11 +0530

    Update Queue/queue2.py

---
+++ Queue/queue2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 20 23:01:19 2019
+
+@author: abhijithneilabraham
+"""
+
+class Queue:
+    def __init__(self,dataval):
+        self.dataval=dataval
+        self.next=None
+        
