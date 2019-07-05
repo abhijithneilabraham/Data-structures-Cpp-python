193f895aa4e66e37e501156df71e69d343176e2b
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-07-05 23:19:02 +0530

    Update hashing/mad_compression.py

--- hashing/mad_compression.py
+++ hashing/mad_compression.py
@@ -5,4 +5,13 @@ Created on Fri Jul  5 23:00:42 2019
 
 @author: abhijithneilabraham
 """
-
+from random import randrange
+#using multiply add and divide compression
+class Mapbase:
+    def __init__(self,cap=11,p=109345121):
+        self.table=cap*[None]
+        self.n=0
+        self.prime=p
+        self.scale=1+randrange(p-1)
+        self.shift=randrange(p)
+        #incomplete
