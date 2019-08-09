97b8749477f1346ed96aa291d986e668701e5360
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-08-09 15:26:47 +0530

    insertionsort update

---
+++ Recursion/parser.java
@@ -0,0 +1,6 @@
+import java.io.*;
+import java.util.*;
+import java.nio.file.*;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+public class parser{
---
+++ sorting algorithms/insertionsorting.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+void insertionsort(int arr[],int n)
+{
+  int i ,key,j;
+  for(i=1;i<n;i++)
+  {
+    key=arr[i];
+    j=i-1;
+while(j>=0 &&arr[j]>key){
+    arr[j+1]=arr[j];
+    j=j-1;
+}
+arr[j+1]=key;
+  }
+}
+void printarray(int arr[],int n){
+  int i;
+  for(i=0;i<n;i++){
+    cout<<"\n array element i="<<arr[i];
+  }
+}
+int main(){
+  int arr[]={ 12, 11, 13, 5, 6 };
+  insertionsort(arr,5);
+  printarray(arr,5);
+}
