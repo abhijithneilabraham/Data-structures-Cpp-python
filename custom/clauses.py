efd0a918bb2997ba1158632572efcc5b2b7e2ac0
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2020-07-08 23:16:58 +0530

    coming soon..

--- custom/clauses.py
+++ custom/clauses.py
@@ -7,20 +7,20 @@ Created on Wed Jul  8 18:47:53 2020
 """
 
 class Clause:
-    def adapt(self,x):
-        print(self.x)
-
-# class Which(Clause):
-#     def __init__(self,q,min_kwd=None,max_kwd=None,avg_kwd=None):
-#         self.min_kwd=None
-#         self.max_kwd=None
-#         self.avg_kwd=None
-#     def adapt(self,x):
-#         print(x)
-#         if self.min_kwd:
-#             return "min"
-#         elif self.min_kwd:
-#             return"max"
+    def __init__(self,q,min_kwd=None,max_kwd=None,avg_kwd=None):
+        self.which_are_clauses=["find","search for","what","get me","which","show"]
+        self.how_many_clauses=["how many","number of","who all","how much","sum of","total"]
+        self.count_clauses=["instances","count"]
+        self.max_clauses=["max","maximum","highest","biggest","most"]
+        self.min_clauses=["min","minimum","lowest","smallest","least"]
+        self.avg_clauses=["average","mean of"]
+        self.inttype=False
+    def adapt(self):
+        print(self.q)
+        if self.min_kwd:
+            return "min"
+        elif self.min_kwd:
+            return"max"
             
-a=Clause.adapt(5) 
+a=Clause(5).adapt() 
         
