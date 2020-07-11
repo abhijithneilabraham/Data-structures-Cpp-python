5c632c2263a1a38b4229f7aba1c4c4f5566435ab
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2020-07-11 19:43:45 +0530

    Update custom/clauses.py

--- custom/clauses.py
+++ custom/clauses.py
@@ -17,16 +17,16 @@ class Clause:
                       ("average","mean of"):"average of {} in {}"
                       }
         self.int_clauses={v[0]:v[1] for i,v in enumerate(self.clauses.items()) if i>2}
-    def adapt(self,q,priority=False):
+    def adapt(self,q,inttype=False,priority=False):
         clauses=self.clauses
         int_clauses=self.int_clauses
 
         for i,tup in enumerate(clauses.items()):
             clause=tup[0]
             if any(i in q for i in clause):
-                if priority  and "how many" in clause:
+                if priority and inttype  and "how many" in clause:
                     return "sum of {} in {}"
-                elif "which" in clause :
+                elif "which" in clause and inttype:
                     for clause2 in int_clauses:
                         if any(i in q for i in clause2):
                             return int_clauses[clause2]
